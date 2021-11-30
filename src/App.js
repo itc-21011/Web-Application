a273a02d24b5690d40c91e869d3fd865cbc248b3
itc-21011 <s21011@std.it-college.ac.jp>
2021-11-30 06:03:23 +0000

    修正

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-const App = props => {
-  const [data, setData] = useState([])
-  const uri = 'https://pokeapi.co/api/v2/pokemon/'
-  useEffect(() => (
-    window.fetch(uri)
-      .then(res => res.json())
-      .then(json => json.results)
-      .then(data => setData(data))
-  ), [])
-  return (
-    <>
-      <ul>
-        {data.map(v => (
-          <li key={v.url}>{v.name}</li>
-        ))}
-      </ul>
-    </>
-  )
+const App = () => {
+  return <h1>hello</h1>
 }
 export default App
