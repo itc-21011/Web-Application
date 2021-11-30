b2505620b01e3a2707f4f56c5b3866f8593b5179
itc-21011 <s21011@std.it-college.ac.jp>
2021-11-30 05:56:34 +0000

    失敗

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
-const App = () => {
-  return <h1>hello</h1>
+const App = props => {
+  const [data, setData] = useState([])
+  const uri = 'https://pokeapi.co/api/v2/pokemon/'
+  useEffect(() => (
+    window.fetch(uri)
+      .then(res => res.json())
+      .then(json => json.results)
+      .then(data => setData(data))
+  ), [])
+  return (
+    <>
+      <ul>
+        {data.map(v => (
+          <li key={v.url}>{v.name}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 export default App
